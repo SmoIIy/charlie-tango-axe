97f0545d6bf71c69628cace5b262f96b709be098
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-23 12:06:12 +0200

    Noget farve

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
+import Main from "@/main/page";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Haiii</h1>
+        <Main />
     </main>
   );
 }
---
+++ src/main/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default async function Main(){
+    return (
+        <section>
+            <h1 className="text-10xl text-orange-500">Diversa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio sit nihil quaerat aliquam recusandae ipsum quia sed maxime, blanditiis iusto incidunt sunt sapiente molestias repellendus placeat corrupti beatae a.</p>
+            <form action="#">
+                <label htmlFor="url">Cock and ball torture</label>
+                <input type="url" name="url" id="url"/>
+                <input type="submit" value="submit" />
+            </form>
+        </section>
+    )
+}
