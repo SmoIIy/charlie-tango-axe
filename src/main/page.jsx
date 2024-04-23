614c1e53f7100781664fa2e41fe6a883dde30391
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-23 12:28:09 +0200

    push piss

--- src/main/page.jsx
+++ src/main/page.jsx
@@ -1,13 +1,13 @@
-
+import "../app/globals.css";
 
 export default async function Main(){
     return (
-        <section>
-            <h1 className="text-10xl text-orange-500">Diversa</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio sit nihil quaerat aliquam recusandae ipsum quia sed maxime, blanditiis iusto incidunt sunt sapiente molestias repellendus placeat corrupti beatae a.</p>
+        <section className="w-40 flex-col ">
+            <h1 className="text-9xl  text-orange-500">Diversa</h1>
+            <p className="underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio sit nihil quaerat aliquam recusandae ipsum quia sed maxime, blanditiis iusto incidunt sunt sapiente molestias repellendus placeat corrupti beatae a.</p>
             <form action="#">
                 <label htmlFor="url">Cock and ball torture</label>
-                <input type="url" name="url" id="url"/>
+                <input className="p-4" type="url" name="url" id="url"/>
                 <input type="submit" value="submit" />
             </form>
         </section>
