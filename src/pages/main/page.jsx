5988c2bb6aef214c906eb782715b6bac002998dc
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-23 13:06:17 +0200

    Update page.jsx

--- src/pages/main/page.jsx
+++ src/pages/main/page.jsx
@@ -2,16 +2,14 @@ import "../../app/globals.css";
 
 export default async function Main() {
   return (
-    <section className="flex max-w-2xl flex-col items-center justify-center p-4">
+    <section className="flex max-w-2xl flex-col items-center justify-center">
       <h1 className="text-9xl font-bold text-orange-500">Diversa</h1>
       <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio sit nihil quaerat aliquam recusandae ipsum quia sed maxime, blanditiis iusto incidunt sunt sapiente molestias repellendus placeat corrupti beatae a.</p>
-      <form action="#" className="border flex flex-col p-4">
-        <div className="p-4 flex">
-            <div className="flex flex-col">
-                <label htmlFor="url">Input Label</label>
-                <input className="p-4 rounded-xl text-black" type="url" name="url" id="url" />
-            </div>
-            <input className="p-4 bg-orange-500 rounded-xl cursor-pointer" type="submit" value="Submit" />
+      <form action="#" className="border flex flex-col">
+        <div className="flex flex-col">
+          <label htmlFor="url">Cock and ball torture</label>
+          <input className="p-4" type="url" name="url" id="url" />
+          <input type="submit" value="submit" />
         </div>
       </form>
     </section>
