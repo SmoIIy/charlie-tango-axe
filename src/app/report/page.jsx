8dff795b94a10c3f81d1e2fc604e8632e4869ee1
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-24 00:48:41 +0200

    formået at hente data fra input

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,9 +1,22 @@
-"use client"
+
 import drreport from "./dr_dk.json"
-export default function Report({ report }){
-    
-    const data =  drreport;
-    console.log(data);
+
+
+
+const params = new URLSearchParams({
+    url: "https://www.charlietango.dk",
+  });
+  const response = await fetch(
+    `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
+  );
+  const data = await response.json();
+
+
+export default async function Report({ report }){
+    //local test env
+    //const data =  drreport;
+    // console.log(data);
+
     return (
         <section>
             <h2>{data.url}</h2>
@@ -11,3 +24,4 @@ export default function Report({ report }){
     )
 
 }
+
