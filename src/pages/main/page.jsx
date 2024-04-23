d0ca30883ec7f0e5514f88a72fa4d11259f37cf7
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-23 12:57:08 +0200

    Update page.jsx

--- src/pages/main/page.jsx
+++ src/pages/main/page.jsx
@@ -5,7 +5,7 @@ export default async function Main() {
     <section className="flex max-w-2xl flex-col items-center justify-center">
       <h1 className="text-9xl font-bold text-orange-500">Diversa</h1>
       <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio sit nihil quaerat aliquam recusandae ipsum quia sed maxime, blanditiis iusto incidunt sunt sapiente molestias repellendus placeat corrupti beatae a.</p>
-      <form action="#" className="border flex flex col">
+      <form action="#" className="border flex flex-col">
         <div className="flex flex-col">
             <label htmlFor="url">Cock and ball torture</label>
             <input className="p-4" type="url" name="url" id="url" />
