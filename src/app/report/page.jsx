bf9a6125814f6ba1c1b1300f6d484acd94efe797
Seb <sebastian.schyberg@gmail.com>
2024-04-24 11:51:39 +0200

    hele lortet virker ikke

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,22 +1,18 @@
-import drreport from "./dr_dk.json"
+import drreport from "./dr_dk.json";
 export const revalidate = 1800;
 
-export default async function Report({ }){
-    //local test env
-    //const data =  drreport;
-    // console.log(data);
-    "use client"
-    const params = new URLSearchParams();
-    const response = await fetch(
-        `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
-    );
-    const data = await response.json();
+export default async function Report({}) {
+  //local test env
+  //const data =  drreport;
+  // console.log(data);
+  "use client";
+  const params = new URLSearchParams();
+  const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
+  const data = await response.json();
 
-    return (
-        <section>
-            <h2>{data.url}</h2>
-        </section>
-    )
-
+  return (
+    <section>
+      <h2>{data.url}</h2>
+    </section>
+  );
 }
-
