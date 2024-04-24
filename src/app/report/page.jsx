c281c610f2b0ac8da5d83938b445c64b34ec3904
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-24 11:51:23 +0200

    Local

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,16 +1,18 @@
-import drreport from "./dr_dk.json"
+
+import drreport from "./dsb_dk.json"
 export const revalidate = 1800;
 
-export default async function Report({ }){
+export default function Report({ }){
     //local test env
-    //const data =  drreport;
-    // console.log(data);
     "use client"
-    const params = new URLSearchParams();
-    const response = await fetch(
-        `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
-    );
-    const data = await response.json();
+    const data =  drreport;
+    console.log(data);
+    // "use client"
+    // const params = new URLSearchParams();
+    // const response = await fetch(
+    //     `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
+    // );
+    // const data = await response.json();
 
     return (
         <section>
