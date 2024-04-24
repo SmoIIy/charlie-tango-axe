3bb103746125859fd1ce8a6bf521bea527dc107b
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-24 11:57:54 +0200

    Breakthrough

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -3,24 +3,6 @@ import "../../app/globals.css";
 
 export default async function Main() {
 
-    //grab data from form
-    async function submit(formUrl){
-        "use server";
-        const url = formUrl.get("url");
-        console.log(url);
-        redirect('/report/?url=' + url);
-    }
-
-
-
-
-
-
-
-
-    
-    
-    
     return (
         <section className="p-16 rounded-3xl shadow-2xl bg-c-grey80">
         <div className="flex flex-col items-center justify-center">
@@ -30,12 +12,12 @@ export default async function Main() {
                 <h2 className="text-c-grey60">Enter your website, and we will test the accesibility <br />No fuss, no ballade - <span className="text-brand-orange70 underline">read more!</span></h2>
             </div>
 
-            <form className="w-full" action={submit}>
+            <form className="w-full" action="/report">
                 <div className="mb-10 space-y-3">
                 <div className="space-y-1">
                     <div className="space-y-2">
                     <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="url">URL</label>
-                    <input className="border-input text-c-grey80 bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 shadow-xl" type="url" id="url" placeholder="website.com..." name="url" />
+                    <input className="border-input text-c-grey80 bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 shadow-xl" type="text" id="url" placeholder="website.com..." name="url" />
                     </div>
                 </div>
     
--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,17 +1,16 @@
 import drreport from "./dr_dk.json"
 export const revalidate = 1800;
 
-export default async function Report({ }){
+export default async function Report({ searchParams }){
     //local test env
     //const data =  drreport;
     // console.log(data);
-    "use client"
-    const params = new URLSearchParams();
+    const params = new URLSearchParams(searchParams);
     const response = await fetch(
         `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
     );
     const data = await response.json();
-
+    console.log(data);
     return (
         <section>
             <h2>{data.url}</h2>
