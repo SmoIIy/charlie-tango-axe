4470654337be375e859e669ae438ba7d08500517
Seb <sebastian.schyberg@gmail.com>
2024-04-29 13:21:34 +0200

    ændringerne ændrede ikke noget

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -3,22 +3,27 @@
 
 import React from "react";
 import { CaretRightOutlined } from "@ant-design/icons";
-import { Collapse } from "antd";
+import { Collapse, theme } from "antd";
 
 export default function Card2({ children, title }) {
+  const { token } = theme.useToken();
   const panelStyle = {
     marginBottom: 24,
     background: "#333333",
     borderRadius: "8px",
     border: "none",
-    colorBorder: "#ffffff", //virker ikke
-    colorText: "#ffffff", //virker ikke
-    colorTextHeading: "#ffffff", //virker ikke
     margin: 24,
   };
 
   return (
-    <Collapse bordered={false} defaultActiveKey={["0"]} expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />} style={{ background: "0" }}>
+    <Collapse
+      bordered={false}
+      defaultActiveKey={["0"]}
+      expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+      style={{
+        background: "0",
+      }}
+    >
       <Collapse.Panel key="1" header={title} style={panelStyle}>
         {children}
       </Collapse.Panel>
