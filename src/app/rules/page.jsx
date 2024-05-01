ce16b8b71845b970aa03a46a3ad5524f971a671e
Seb <sebastian.schyberg@gmail.com>
2024-05-02 01:09:27 +0200

    dumb fix *with rizz*

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -3,43 +3,89 @@
 import "../../app/globals.css";
 import { redirect } from "next/navigation";
 import React from "react";
-import { Card, List } from "antd";
+import { Card, List, ConfigProvider } from "antd";
 
 const data = [
   {
     title: "Title 1",
+    content: "test",
   },
   {
     title: "Title 2",
+    content: "test",
   },
   {
     title: "Title 3",
+    content: "test",
   },
   {
     title: "Title 4",
+    content: "test",
   },
   {
     title: "Title 5",
+    content: "test",
   },
   {
     title: "Title 6",
+    content: "test",
+  },
+  {
+    title: "Title 7",
+    content: "test",
+  },
+  {
+    title: "Title 8",
+    content: "test",
+  },
+  {
+    title: "Title 9",
+    content: "test",
+  },
+  {
+    title: "Title 10",
+    content: "test",
+  },
+  {
+    title: "Title 11",
+    content: "test",
+  },
+  {
+    title: "Title 12",
+    content: "test",
+  },
+  {
+    title: "Title 13",
+    content: "test",
+  },
+  {
+    title: "Title 14",
+    content: "test",
+  },
+  {
+    title: "Title 15",
+    content: "test",
   },
 ];
 const App = () => (
   <List
     grid={{
-      gutter: 16,
+      gutter: 24,
       xs: 1,
-      sm: 2,
-      md: 4,
-      lg: 4,
-      xl: 6,
+      sm: 1,
+      md: 2,
+      lg: 2,
+      xl: 2,
       xxl: 3,
     }}
     dataSource={data}
     renderItem={(item) => (
       <List.Item>
-        <Card title={item.title}>Card content</Card>
+        <a href="">
+          <Card title={item.title} content={item.content}>
+            {item.content}
+          </Card>
+        </a>
       </List.Item>
     )}
   />
