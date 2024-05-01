5dbf2417ddefbd28865e88b040bf5e407d39ca93
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-01 14:36:57 +0200

    Textbox.jsx

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -3,8 +3,9 @@
 export default async function Textbox(){
     return (
         <article className="my-4 mx-8 p-8 bg-c-grey80 rounded-xl" >
-            <h2 className="text-xl text-brand-orange70">Text</h2>
-            <p className="max-w-prose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis facere ex officiis officia quae consequatur reiciendis, soluta cum eum sit nihil beatae fuga. Modi reprehenderit esse, odit iure magnam minus.</p>
+            <h2 className="text-xl font-bold text-brand-orange70 leading-relaxed">Description</h2>
+            <p className="max-w-prose">When you submit a website URL to Diversa, our system automatically scans the site using the Deque Axe ruleset. It evaluates each element of the website against these rules and generates a detailed report. Based on the results, we calculate a score that reflects the overall accessibility and usability of the website.</p>
         </article>
+       
     )
 }
