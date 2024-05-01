2c23f2d4d356847a2b70572bea4f07fad69b0e84
Seb <sebastian.schyberg@gmail.com>
2024-05-01 09:06:53 +0200

    page created

---
+++ src/app/rules/page.jsx
@@ -0,0 +1,10 @@
+import "../../app/globals.css";
+import { redirect } from "next/navigation";
+
+export default async function Rules() {
+  return (
+    <div>
+      <h1>Hello World..</h1>
+    </div>
+  );
+}
