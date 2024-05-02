9783494ae12ddfd61fceb110054e8ef7d4bc85e4
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-02 13:40:33 +0200

    Interaktiv rulepage

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -3,23 +3,26 @@ import Rules from "./rules.json";
 
 const RuleCard = ({ id, title, content }) => (
   <Link href={`/rules/${id}`}>
-    <div className="cursor-pointer bg-c-grey80 rounded-md shadow-md p-6 mb-4">
+    <article className="cursor-pointer bg-c-grey80 rounded-md mx-2 sm:mx-0 p-6 mb-4 max-w-lg h-full border border-c-grey80 hover:border-brand-orange70 shadow-inner hover:bg-c-grey90 transition-all duration-300">
       <h2 className="text-lg text-brand-orange70 font-semibold mb-2">{title}</h2>
-      <p>{content}</p>
-    </div>
+      <p className="max-w-prose">{content}</p>
+    </article>
   </Link>
 );
 
 const Rulepage = () => {
   return (
-    <div className="m-4">
-      <h1 className="text-4xl font-bold mb-4 text-brand-orange70">Rules</h1>
-      <div>
-        {Rules.map((rule, index) => (
-          <RuleCard key={index} id={rule.id} title={rule.title} content={rule.content} />
-        ))}
-      </div>
-    </div>
+   <>
+        <h1 className="text-4xl font-bold mb-4 text-brand-orange70 text-center">Rules</h1>
+        <section className="m-4 mx-auto max-w-6xl flex justify-center flex-wrap gap-4 items-stretch">
+          
+    
+            {Rules.map((rule, index) => (
+              <RuleCard key={index} id={rule.id} title={rule.title} content={rule.content} />
+            ))}
+    
+        </section>
+   </>
   );
 };
 
