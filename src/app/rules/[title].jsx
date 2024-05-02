6d3da3dbcea905fcc132aaece9d773791bf849f7
Seb <sebastian.schyberg@gmail.com>
2024-05-02 12:55:56 +0200

    id men det virker ikke

--- src/app/rules/[title].jsx
+++ src/app/rules/[title].jsx
@@ -2,10 +2,9 @@ import { useRouter } from "next/router";
 import Rules from "./rules.json";
 import Link from "next/link";
 
-const RuleDetail = () => {
-  const { title } = router.query;
-  const decodedTitle = decodeURIComponent(title);
-  const rule = Rules.find((rule) => rule.title === decodedTitle);
+const RuleDetailPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
   if (!rule) return <div>Rule not found</div>;
 
