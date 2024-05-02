4e2e3b19902921b235435f508580e56c2db3c956
Seb <sebastian.schyberg@gmail.com>
2024-05-02 15:14:38 +0200

    rule page :DD

--- src/app/rules/[id]/page.jsx
+++ src/app/rules/[id]/page.jsx
@@ -1,5 +1,30 @@
+import React from "react";
 import Rules from "../../rules/rules.json";
 
-export default async function Page() {
-  return <h1>Placeholder</h1>;
-}
+const Page = () => {
+  // Assuming you're fetching a rule with id '1' as an example
+  const rule = Rules.find((rule) => rule.id === "1");
+
+  return (
+    <div className="p-8">
+      {rule && (
+        <div className="max-w-lg mx-auto bg-c-grey80 shadow-md rounded-lg overflow-hidden">
+          <div className="p-6">
+            <h1 className="text-2xl font-bold text-brand-orange70 mb-4">{rule.title}</h1>
+            <h2 className="text-lg font-bold mb-2 text-brand-orange70">Impact:</h2>
+            <p className="text-gray-300 mb-4">{rule.impact}</p>
+            <p className="text-gray-300 mb-4">{rule.content}</p>
+            <h2 className="text-lg font-bold mb-2 text-brand-orange70">How to Fix:</h2>
+            <p className="text-gray-300 mb-4">{rule.howToFix}</p>
+            <h2 className="text-lg font-bold mb-2 text-brand-orange70">Note:</h2>
+            <p className="text-gray-300 mb-4">{rule.note}</p>
+            <h2 className="text-lg font-bold mb-2 text-brand-orange70">Description:</h2>
+            <p className="text-gray-300 mb-4">{rule.description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Page;
