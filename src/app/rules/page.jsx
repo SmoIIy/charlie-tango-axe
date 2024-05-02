2993dc6d52499e54bac4e464b014df8c6a8edd02
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-02 11:58:17 +0200

    Update page.jsx

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -1,1 +1,11 @@
 // det var en fallout reference
+import Rules from "./rules.json";
+const data = Rules;
+
+export default async function Rulepage(){
+
+    console.log(data);
+    return (
+        <h1>h1</h1>
+    )
+}
